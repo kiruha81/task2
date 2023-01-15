4820c0218626e72111a997d3bc3e9dc920a0ccb5
EC2 Default User <ec2-user@ip-172-31-4-186.ap-northeast-1.compute.internal>
2023-01-15 08:21:09 +0000

    [finish]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created"
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @booklists = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
-    @allbooks = Book.new
+    @book = Book.new
     @booklists = Book.all
   end
 
@@ -19,21 +23,25 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.new
     @booklist = Book.find(params[:id])
-    @booklists = Book.all
   end
 
   def update
     @booklist = Book.find(params[:id])
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
